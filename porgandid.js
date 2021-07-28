86455889155f8a8ff9c7763e7800cae0f35fb2ce
Maarja <mahoop@ttu.ee>
2021-07-28 12:28:27 +0300

    initial files added

---
+++ porgandid.js
@@ -0,0 +1,5 @@
+function vihje() {
+    return {"hint": "Loe hoolega läbi, vaata üht ja teistpidi. Näed allpoolne tekst vajab tõlkimist veidi.",
+            "data": "MINGI TEKST",
+            "encrypt type": "ROT13"}
+  }
