2f10b1ff2cae8efa21377d9db84dc24aa155e2fc
Maarja <mahoop@ttu.ee>
2021-07-29 22:18:35 +0300

    roughly olemas

--- esimene-task/porgandid.js
+++ esimene-task/porgandid.js
@@ -1,5 +1,5 @@
 function vihje() {
     return {"hint": "Loe hoolega läbi, vaata üht ja teistpidi. Näed allpoolne tekst vajab tõlkimist veidi.",
-            "data": "MINGI TEKST",
-            "encrypt type": "ROT13"}
+            "data": "ivuwr2.vghx.rr",
+            "encrypt-type": "ROT13"}
   }
